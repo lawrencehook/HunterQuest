bba75dc50ed2c388e6189be0129aebb720d39a17
Steven Wang <slw3bm@virginia.edu>
2017-03-27 22:08:19 -0400

    Initial projectile implementation (incomplete)

---
+++ javascript/src/edu/virginia/engine/display/Projectile.js
@@ -0,0 +1,53 @@
+// Steven Wang
+
+"use strict";
+
+class Platform extends DisplayObjectContainer {
+	constructor(x, y, width, height, vx, vy, damage, color="#2f4d2f", parentObj=null) {
+		super("platform", "", parentObj);
+		this.position = new Point(x, y);
+		this.width = width;
+		this.height = height;
+		this.vx = vx;
+		this.vy = vy;
+		this.damage = damage;
+		this.fillColor = color;
+
+		this.collidable = true;
+	}
+
+	update(pressedKeys) {
+		super.update(pressedKeys);
+
+		this.position.translate(vx, vy);
+	}
+
+	draw(context) {
+		this.applyTransformations(context);
+		context.beginPath();
+		context.rect(0, 0, this.width, this.height);
+		
+		context.fillStyle = this.fillColor;
+		context.fill();
+		
+		context.strokeStyle = this.fillColor;
+		context.stroke();
+		this.reverseTransformations(context);
+	}
+
+	applyTransformations(context) {
+		context.save();
+		context.translate(...this.position.toA());
+	}
+
+	reverseTransformations(context) {
+		context.restore();
+	}
+
+	getUnscaledWidth() {
+		return this.width;
+	}
+	getUnscaledHeight() {
+		return this.height;
+	}
+}
