de6d9024d9f4850e04b9ab2decc7534fecd931a9
Jon Ting <jting416@gmail.com>
2017-04-20 20:19:14 -0400

    Added attack pattern (spray) for final boss

--- src/edu/virginia/hunterquest/HunterQuest.js
+++ src/edu/virginia/hunterquest/HunterQuest.js
@@ -88,7 +88,7 @@ class HunterQuest extends Game {
 		this.levels.push(new LevelThree());
 		this.levels.push(new LevelFour());
 
-		// this.levels.push(new LevelFive());
+		this.levels.push(new LevelFive());
 		// this.levels.push(new LevelSix());
 		// this.levels.push(new LevelSeven());
 		// this.levels.push(new LevelEight());
