c7ef52d3ef7c969296b2104d8161c79791ca7b05
Jon Ting <jting416@gmail.com>
2017-04-24 16:34:07 -0400

    Added last two level (8 & 9) and changed level complete message

--- src/edu/virginia/hunterquest/levels/LevelEight.js
+++ src/edu/virginia/hunterquest/levels/LevelEight.js
@@ -8,42 +8,44 @@ class LevelEight extends Level {
 	initialize() {
 		super.initialize();
 		var game = Game.getInstance();
-		var simon1 = new Monster("enemy1", "monster/simon.png", null, game.gamescreen, 100);
 
 		var screenWidth = game.gamescreen.width - 50;
 		var screenHeight = game.gamescreen.height - 50;
 
-		simon1.position = game.midPoint.minus(new Point(75, 0));
+		var bigMonster = new Monster("enemy1", "monster/steve.png", null, game.gamescreen);
+		bigMonster.position = game.midPoint.minus(new Point(0, (game.gamescreen.height/2)-75));
 
-		simon1.attackSpeed = 1;
-		simon1.projectileSpeed = 5;
-		simon1.projectileSize = 5;
-		simon1.projectileDamage = 1;
-		simon1.projectileColor = "#130951";
+		bigMonster.attackSpeed = 100;
+		bigMonster.projectileSpeed = 5;
+		bigMonster.projectileSize = 15;
+		bigMonster.projectileDamage = 15;
+		bigMonster.projectileColor = "#963c2b";
+		bigMonster.projectileTracking = 0.035;
 
-		simon1.hp = 25;
-		simon1.maxHealth = 25;
+		bigMonster.hp = 50;
+		bigMonster.maxHealth = 50;
 
-		var simon2 = new Monster("enemy1", "monster/simon.png", null, game.gamescreen, 100)
+		var bigMonster2 = new Monster("enemy1", "monster/steve.png", null, game.gamescreen)
 
-		simon2.position = game.midPoint.plus(new Point(75, 0));
+		bigMonster2.position = game.midPoint.minus(new Point(0, -(game.gamescreen.height/2)+75));
 
-		simon2.attackSpeed = 1;
-		simon2.projectileSpeed = 5;
-		simon2.projectileSize = 5;
-		simon2.projectileDamage = 1;
-		simon2.projectileColor = "#130951";
+		bigMonster2.attackSpeed = 100;
+		bigMonster2.projectileSpeed = 5;
+		bigMonster2.projectileSize = 15;
+		bigMonster2.projectileDamage = 15;
+		bigMonster2.projectileColor = "#963c2b";
+		bigMonster2.projectileTracking = 0.035;
 
-		simon2.hp = 25;
-		simon2.maxHealth = 25;
+		bigMonster2.hp = 50;
+		bigMonster2.maxHealth = 50;
 
-		for (var i = 0; i < 3; i++) {
+		for (var i = 0; i < 5; i++) {
 			this.monsters[i] = new Monster("enemy1", "monster/wyatt.png", null, game.gamescreen);
 			this.monsters[i].position = (new Point(Math.random() * screenWidth, Math.random() * screenHeight));
 		}
 
-		this.monsters.push(simon1);
-		this.monsters.push(simon2);
+		this.monsters.push(bigMonster);
+		this.monsters.push(bigMonster2);
 
 	}
 }
--- src/edu/virginia/hunterquest/levels/LevelNine.js
+++ src/edu/virginia/hunterquest/levels/LevelNine.js
@@ -37,7 +37,34 @@ class LevelNine extends Level {
 		simon2.hp = 25;
 		simon2.maxHealth = 25;
 
-		for (var i = 0; i < 3; i++) {
+		var bigMonster = new Monster("enemy1", "monster/steve.png", null, game.gamescreen);
+		bigMonster.position = game.midPoint.minus(new Point(0, (game.gamescreen.height/2)-75));
+
+		bigMonster.attackSpeed = 100;
+		bigMonster.projectileSpeed = 5;
+		bigMonster.projectileSize = 15;
+		bigMonster.projectileDamage = 15;
+		bigMonster.projectileColor = "#963c2b";
+		bigMonster.projectileTracking = 0.035;
+
+		bigMonster.hp = 50;
+		bigMonster.maxHealth = 50;
+
+		var bigMonster2 = new Monster("enemy1", "monster/steve.png", null, game.gamescreen)
+
+		bigMonster2.position = game.midPoint.minus(new Point(0, -(game.gamescreen.height/2)+75));
+
+		bigMonster2.attackSpeed = 100;
+		bigMonster2.projectileSpeed = 5;
+		bigMonster2.projectileSize = 15;
+		bigMonster2.projectileDamage = 15;
+		bigMonster2.projectileColor = "#963c2b";
+		bigMonster2.projectileTracking = 0.035;
+
+		bigMonster2.hp = 50;
+		bigMonster2.maxHealth = 50;
+
+		for (var i = 0; i < 5; i++) {
 			this.monsters[i] = new Monster("enemy1", "monster/wyatt.png", null, game.gamescreen);
 			this.monsters[i].position = (new Point(Math.random() * screenWidth, Math.random() * screenHeight));
 		}
